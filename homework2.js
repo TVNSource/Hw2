2c4645d7662c05fa9f54064f4ae454d108c2f60c
TVNSource <vanthinh@gmail.com>
2024-10-10 22:49:50 -0500

    add validate DOB
    Add validate DOB
    Add class for errorMsg and validMsg

--- homework2.js
+++ homework2.js
@@ -10,9 +10,41 @@
  document.getElementById("today").innerHTML = 'Today date is: '+new Date().toLocaleDateString();
  
  //code for slider
- var slider = document.getElementById("healthBar");
- var output = document.getElementById("healthBarVal");
-   	 output.innerHTML = slider.value;
-	 slider.oninput = function() {output.innerHTML = this.value;}
+ let slider = document.getElementById("healthBar");
+ let output = document.getElementById("healthBarVal");
+	output.innerHTML = slider.value;
+	slider.oninput = function() {output.innerHTML = this.value;}
+	 
+	 
+//don't allow dates in the future or more than 120 years ago
+function checkDate(){
+	
+	let inputDate = document.getElementById("DOB").value;
+	
+	let [year, month, day] = inputDate.split('-');
+	let myDate = new Date(year, month - 1, day);  // Month is zero-based in JS Date
+	
+	const currentDate = new Date();		
+    var minDate = new Date();	
+	minDate.setFullYear(minDate.getFullYear() - 120);	
+	
+	const msgLabel = document.getElementById("DOB_msg");
+	
+	if(myDate > currentDate){		
+		msgLabel.className = 'errorMsg';
+		msgLabel.innerHTML = "Invalid Date. Date in future.";
+	}
+	else if( myDate < minDate){		
+		msgLabel.className = 'errorMsg';
+		msgLabel.innerHTML = "Invalid Date. The date is older than 120 years.";
+	}
+	else{
+		msgLabel.className = 'validMsg';
+		msgLabel.innerHTML = "Valid Date";		
+	}	
+		
+   
+
+}
 
  
